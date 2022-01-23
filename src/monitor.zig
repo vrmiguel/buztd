c081f47fe1319fa8b830870a5da2b7ea7482251a
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2022-01-22 22:46:18 -0300

    feat: add adaptive sleep time

--- src/monitor.zig
+++ src/monitor.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const time = std.time;
+const math = std.math;
 
 const memory = @import("memory.zig");
 const pressure = @import("pressure.zig");
@@ -9,11 +10,11 @@ const MemoryStatusTag = enum {
     near_terminal,
 };
 
-const MemoryStatus = union(MemoryStatusTag) { 
+const MemoryStatus = union(MemoryStatusTag) {
     /// Memory is "okay": basically no risk of memory thrashing 
-    ok: void, 
+    ok: void,
     /// Nearing the terminal PSI cutoff: memory thrashing is occurring or close to it. Holds the current PSI value.
-    near_terminal: f32 
+    near_terminal: f32
 };
 
 pub const Monitor = struct {
@@ -57,8 +58,49 @@ pub const Monitor = struct {
             std.log.warn("Free RAM = {}%", .{self.mem_info.available_ram_percent});
             try self.updateMemoryStats();
             std.log.warn("isMemoryLow = {}", .{self.isMemoryLow()});
-            time.sleep(200_000);
+            time.sleep(self.sleepTimeNs());
         }
+    }
+
+    /// Determines how much bustd should sleep
+    /// This function is essentially a copy of how earlyoom calculates its sleep time
+    ///
+    /// Credits: https://github.com/rfjakob/earlyoom/blob/dea92ae67997fcb1a0664489c13d49d09d472d40/main.c#L365
+    /// MIT Licensed
+    fn sleepTimeNs(self: *const Self) u64 {
+        // Maximum expected memory fill rate as seen
+        // with `stress -m 4 --vm-bytes 4G`
+        const ram_fill_rate: i64 = 6000;
+        // Maximum expected swap fill rate as seen
+        // with membomb on zRAM
+        const swap_fill_rate: i64 = 800;
+
+        // Maximum and minimum sleep times (in ms)
+        const min_sleep: i64 =  100;
+        const max_sleep: i64 = 1000;
+
+        // TODO: make these percentages configurable by args./config. file
+        const ram_terminal_percent: f64 = 10.0;
+        const swap_terminal_percent: f64 = 10.0;
+
+
+        const f_ram_headroom_kib = (@intToFloat(f64, self.mem_info.available_ram_percent)
+            - ram_terminal_percent)
+            * (@intToFloat(f64, self.mem_info.total_ram_mb) * 10.0);
+        const f_swap_headroom_kib = (@intToFloat(f64, self.mem_info.available_swap_percent)
+            - swap_terminal_percent)
+            * (@intToFloat(f64, self.mem_info.total_swap_mb) * 10.0);
+
+        const i_ram_headroom_kib = math.max(0, @floatToInt(i64, f_ram_headroom_kib));
+        const i_swap_headroom_kib = math.max(0, @floatToInt(i64, f_swap_headroom_kib));
+
+
+        var time_to_sleep = @divFloor(i_ram_headroom_kib, ram_fill_rate) + @divFloor(i_swap_headroom_kib, swap_fill_rate);
+        time_to_sleep = math.min(time_to_sleep, max_sleep);
+        time_to_sleep = math.max(time_to_sleep, min_sleep);
+        
+        // Convert ms to ns
+        return @intCast(u64, time_to_sleep) * 1000000;
     }
 
     fn isMemoryLow(self: *const Self) bool {
